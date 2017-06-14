6a8d09294e1968c0d1edcdfe43e5b93f6ef624a7
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-06-14 14:25:33 -0400

    so much fun when the code works but the tests dont pass locally

--- public/js/cars.js
+++ public/js/cars.js
@@ -1,22 +1,31 @@
 "use strict";
 
 // this is the base API url
-var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/";
+var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/"
+var count = 3
 
 function formatCars(carsJSON) {
-  // this function shold return a string of properly formatted html
-  // refer to app/views/index.erb lines 16 - 22 for an example of how
-  // to format three cars, each in a div with a class "col-md-4", in a 
-  // div with a class "row"
+  return (
+    '<div class="row">' +
+    carsJSON.map(c => {
+      return `<div class="col-md-4 car"><h2>${c.Make}</h2><p><strong>Model:</strong> ${c.Model}</p><p><strong>Year:</strong> ${c.Year}</p></div>`
+    }).join('')
+    + '</div>'
+  )
 }
 
 function addCarsToDOM(carsJSON) {
-  // this function should pass carsJSON to formatCars() and then 
-  // add the resulting HTML to the div with an id of "cars"
+  let input = formatCars(carsJSON)
+  $('#cars').append(input)
 }
 
 function fetchJSON() {
-  // this function will make the ajax call
-  // on success of the ajax call, it will pass the returned data
-  // to addCarsToDOM()
+  $.ajax({
+    url: `${baseUrl}${count}/3`,
+    dataType: 'jsonp',
+    success: function (data) {
+      addCarsToDOM(data)
+    }
+  })
+  count++
 }
